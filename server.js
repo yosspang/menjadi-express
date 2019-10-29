f0e24caf996dfefa8518d4a80da6ea3be18e437e
Yosua <yosspang95@gmail.com>
2019-10-29 10:35:45 +0700

    membuat request post

--- server.js
+++ server.js
@@ -5,5 +5,15 @@ const port = 3007
 
 app.get('/', (req,res) => res.send('Hewwo World!')) // req request response
 
+//membuat request post
+app.post('/hello', function(req,res){
+    const respon = {
+        statusCode: 200,
+        error: "",
+        message: "hello json"
+    }
+    res.json(respon);
+})
+//commit lagi dengan nama "membuat request post"
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
